97e9c8dbbb28b505cd4a8c58378dabc8f13d64e8
Pan-Mengyu <68075614+Pan-Mengyu@users.noreply.github.com>
2021-02-18 18:39:01 +0800

    Update td1d.cpp

--- ROB305/TD1/td1d.cpp
+++ ROB305/TD1/td1d.cpp
@@ -3,22 +3,38 @@
 #include <sstream>
 #include <climits>
 
+unsigned int a=0;
+unsigned int b=0;
+unsigned int temp=0;
+
+void calib(unsigned int I){
+	a = (I - temp)/2;
+	b = I - 6*a;
+	printf("a=%d,b=%d\n",a,b);
+}
+
 void callback(timespec start,timespec end,bool* pStop){
-	if((end.tv_sec-start.tv_sec)>4){
+	if((end.tv_sec-start.tv_sec)>6){
 		*pStop = true;
 	}
 }
 
 void incr(unsigned int nLoops, double* pCounter,bool* pStop){
 	timespec start,end,time;
+	unsigned int i = 0;
 	//start timing
 	clock_gettime(CLOCK_REALTIME,&start);
- 	for(unsigned int i=0;(i<nLoops)&&(*pStop==false);i++){
+ 	for(i=0;(i<nLoops)&&(*pStop==false);i++){
  		*pCounter += 1.0;
  		//end timing
  		clock_gettime(CLOCK_REALTIME,&end);
- 		callback(start,end,pStop); 		
+ 		callback(start,end,pStop);
+ 		if((end.tv_sec-start.tv_sec)==4){
+ 		 	temp = i;
+ 		} 		
 	}
+
+	calib(i);
 
 	//calculate time
 	if(end.tv_nsec<start.tv_nsec){
