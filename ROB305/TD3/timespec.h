65ef1eb08f63f702d30199b72c270fd5f7313a2c
Pan-Mengyu <68075614+Pan-Mengyu@users.noreply.github.com>
2021-03-04 18:30:55 +0800

    Update timespec.h

--- ROB305/TD3/timespec.h
+++ ROB305/TD3/timespec.h
@@ -8,3 +8,18 @@ timespec timespec_negate(const timespec& time_ts);
 timespec timespec_add(const timespec& time1_ts, const timespec& time2_ts);
 timespec timespec_subtract(const timespec& time1_ts, const timespec& time2_ts);
 timespec timespec_wait(const timespec& delay_ts);
+
+class Operator_reload{
+	public:
+		timespec time1_ts, time2_ts;
+		timespec  operator- (const timespec& time_ts);
+		timespec  operator+ (const timespec& time1_ts, const timespec& time2_ts);
+		timespec  operator- (const timespec& time1_ts, const timespec& time2_ts);
+		timespec& operator+= (timespec& time_ts, const timespec& delay_ts);
+		timespec& operator-= (timespec& time_ts, const timespec& delay_ts);
+		bool operator== (const timespec& time1_ts, const timespec& time2_ts);
+		bool operator!= (const timespec& time1_ts, const timespec& time2_ts);
+		bool operator< (const timespec& time1_ts, const timespec& time2_ts);
+		bool operator> (const timespec& time1_ts, const timespec& time2_ts);
+}
+
