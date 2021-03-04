7023614d82a49fa2c1a8be4b6ad53eac8221df03
Pan-Mengyu <68075614+Pan-Mengyu@users.noreply.github.com>
2021-03-04 18:21:44 +0800

    Create timespec.h

---
+++ ROB305/TD3/timespec.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <time.h>
+
+double timespec_to_ms(const timespec& time_ts);
+timespec timespec_from_ms(double time_ms);
+timespec timespec_now();
+timespec timespec_negate(const timespec& time_ts);
+timespec timespec_add(const timespec& time1_ts, const timespec& time2_ts);
+timespec timespec_subtract(const timespec& time1_ts, const timespec& time2_ts);
+timespec timespec_wait(const timespec& delay_ts);
