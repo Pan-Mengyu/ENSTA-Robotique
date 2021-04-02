b7c6e1b33c9a6b814a6f105879665390184f16b3
Pan-Mengyu <68075614+Pan-Mengyu@users.noreply.github.com>
2021-04-02 23:20:04 +0200

    Update timespec.cpp

--- ROB305/TD3/timespec.cpp
+++ ROB305/TD3/timespec.cpp
@@ -1,123 +1,126 @@
-#include <iostream>
 #include <time.h>
+#include <cerrno>
 #include "timespec.hpp"
 
-timespec timespec_from_ms(double time_ms){
-	timespec time;
-	time.tv_sec = int(time_ms)/1000;
-	time.tv_nsec = (time_ms*1000*1000-int(time_ms)*1000*1000);
-	return time;
+
+double timespec_to_ms(const timespec& time_ts)
+{
+	double time_ms = 0.0;
+	time_ms = time_ts.tv_sec*1000 + time_ts.tv_nsec/1e6;
+	return time_ms;
 }
 
-double timespec_to_ms(const timespec& time_ts){
-	double time;
-	time = time_ts.tv_sec*1000+double(time_ts.tv_nsec)/1000/1000;
-	return time;	
-}
-
-timespec timespec_now(){
-	timespec time_now;
-	clock_gettime(CLOCK_REALTIME, &time_now);
-	return time_now;
-}
-
-timespec timespec_negate(const timespec& time_ts){
-	timespec time;
-	time.tv_sec = -time_ts.tv_sec;
-	time.tv_nsec = - time_ts.tv_nsec;
-	return time;
-}
-
-timespec timespec_add(const timespec& time1_ts, const timespec& time2_ts){
-	timespec time;
-	time.tv_sec = time1_ts.tv_sec+time2_ts.tv_sec;
-	time.tv_nsec = time1_ts.tv_nsec+time2_ts.tv_nsec;
-	return time;
-}
-
-timespec timespec_subtract(const timespec& time1_ts, const timespec& time2_ts){
-	timespec time;
-	time.tv_sec = time1_ts.tv_sec-time2_ts.tv_sec;
-	time.tv_nsec = time1_ts.tv_nsec-time2_ts.tv_nsec;
-	return time;
-}
-
-timespec timespec_wait(const timespec& delay_ts){
-	nanosleep(&delay_ts,NULL);
-	return delay_ts;
-}
-
-timespec Operator_reload::operator-(const timespec& time_ts){
-	timespec time;
-	time.tv_sec = -time_ts.tv_sec;
-	time.tv_nsec = - time_ts.tv_nsec;
-	return time;
-}
-timespec Operator_reload::operator+(const timespec& time1_ts,const timespec& time2_ts){
-	timespec time;
-	time.tv_sec = time1_ts.tv_sec+time2_ts.tv_sec;
-	time.tv_nsec = time1_ts.tv_nsec+time2_ts.tv_nsec;
-	return time;
-}
-timespec Operator_reload::operator-(const timespec& time1_ts,const timespec& time2_ts){
-	timespec time;
-	time.tv_sec = time1_ts.tv_sec-time2_ts.tv_sec;
-	time.tv_nsec = time1_ts.tv_nsec-time2_ts.tv_nsec;
-	return time;
-}
-timespec Operator_reload::operator+=(const timespec& time_ts,const timespec& delay_ts){
-	timespec time;
-	time.tv_sec = time_ts.tv_sec+delay_ts.tv_sec;
-	time.tv_nsec = time_ts.tv_nsec+delay_ts.tv_nsec;
-	return time;
-}
-timespec Operator_reload::operator-=(const timespec& time_ts,const timespec& delay_ts){
-	timespec time;
-	time.tv_sec = time_ts.tv_sec-delay_ts.tv_sec;
-	time.tv_nsec = time_ts.tv_nsec-delay_ts.tv_nsec;
-	return time;
-}
-bool Operator_reload::operator==(const timespec& time1_ts,const timespec& time2_ts){
-	bool flag = false;
-	if (time1_ts.tv_sec==time2_ts.tv_sec)&&(time1_ts.tv_nsec==time2_ts.tv_nsec){
-		flag = true;
+timespec timespec_from_ms(double time_ms)
+{
+	struct timespec time_ts;
+	time_ts.tv_sec = time_ms/1e3;
+	if (time_ms < 0){
+		time_ts.tv_sec -= 1;
 	}
-	return flag;
+	time_ts.tv_nsec = (time_ms - time_ts.tv_sec*1e3)*1e6;
+	return time_ts;
 }
-bool Operator_reload::operator!=(const timespec& time1_ts,const timespec& time2_ts){
-	bool flag = true;
-	if (time1_ts.tv_sec==time2_ts.tv_sec)&&(time1_ts.tv_nsec==time2_ts.tv_nsec){
-		flag = false;
-	}
-	return flag;
+
+timespec timespec_now()
+{
+	struct timespec now;
+	clock_gettime(CLOCK_REALTIME, &now);
+	return now;
 }
-bool Operator_reload::operator<(const timespec& time1_ts,const timespec& time2_ts){
-	bool flag = true;
-	if(time1_ts.tv_sec>time2_ts.tv_sec){
-		flag = false;
-	}
-	else if (time1_ts.tv_sec==time2_ts.tv_sec){
-		if(time1_ts.tv_nsec>=time2_ts.tv_nsec){
-			flag = false;
-		}
+
+timespec timespec_negate(const timespec& time_ts)
+{
+	struct timespec time_neg;
+	if (time_ts.tv_nsec == 0){
+		time_neg.tv_sec = -time_ts.tv_sec;
+		time_neg.tv_nsec = 0;
 	}
 	else{
-		flag = true;
+		time_neg.tv_sec = -time_ts.tv_sec - 1;
+		time_neg.tv_nsec = 1e9 - time_ts.tv_nsec;
 	}
-	return flag;
+	return time_neg;
 }
-bool Operator_reload::operator>(const timespec& time1_ts,const timespec& time2_ts){
-	bool flag = true;
-	if(time1_ts.tv_sec<time2_ts.tv_sec){
-		flag = false;
-	}
-	else if (time1_ts.tv_sec==time2_ts.tv_sec){
-		if(time1_ts.tv_nsec<=time2_ts.tv_nsec){
-			flag = false;
+
+timespec timespec_add(const timespec& time1_ts, const timespec& time2_ts)
+{
+	struct timespec time_sum;                                       
+    time_sum.tv_sec = time1_ts.tv_sec + time2_ts.tv_sec;
+    time_sum.tv_nsec = time1_ts.tv_nsec + time2_ts.tv_nsec;
+    if(time_sum.tv_nsec >= 1e9){
+        time_sum.tv_sec += 1;
+        time_sum.tv_nsec -= 1e9;
+    }
+    return time_sum;
+}
+
+timespec timespec_subtract(const timespec& time1_ts, const timespec& time2_ts)
+{
+    struct timespec time_sub;
+    time_sub.tv_sec = time1_ts.tv_sec - time2_ts.tv_sec;
+    if(time1_ts.tv_nsec - time2_ts.tv_nsec < 0)
+    {
+        time_sub.tv_sec -= 1;
+        time_sub.tv_nsec = time1_ts.tv_nsec - time2_ts.tv_nsec + 1e9;
+    }
+    else {time_sub.tv_nsec = time1_ts.tv_nsec - time2_ts.tv_nsec;}
+    return time_sub;
+}
+
+timespec timespec_wait(const timespec& delay_ts)
+{
+	struct timespec rem_ts;
+	if (nanosleep(&delay_ts, &rem_ts) == -1){
+		if (errno == EINTR){
+			timespec_wait(rem_ts);
 		}
 	}
-	else{
-		flag = true;
-	}
-	return flag;
+	return rem_ts;
+}
+
+timespec  operator- (const timespec& time_ts)
+{
+	return timespec_negate(time_ts);
+}
+
+timespec  operator+ (const timespec& time1_ts, const timespec& time2_ts)
+{
+	return timespec_add(time1_ts, time2_ts);
+}
+
+timespec  operator- (const timespec& time1_ts, const timespec& time2_ts)
+{
+	return timespec_subtract(time1_ts, time2_ts);
+}
+
+timespec& operator+= (timespec& time_ts, const timespec& delay_ts)
+{
+	time_ts = timespec_add(time_ts, delay_ts);
+	return time_ts;
+}
+
+timespec& operator-= (timespec& time_ts, const timespec& delay_ts)
+{
+	time_ts = timespec_subtract(time_ts, delay_ts);
+	return time_ts;
+}
+
+bool operator== (const timespec& time1_ts, const timespec& time2_ts)
+{
+	return (time1_ts.tv_sec == time2_ts.tv_sec && time1_ts.tv_nsec == time2_ts.tv_nsec);
+}
+
+bool operator!= (const timespec& time1_ts, const timespec& time2_ts)
+{
+	return (time1_ts.tv_sec != time2_ts.tv_sec || time1_ts.tv_nsec != time2_ts.tv_nsec);
+}
+
+bool operator< (const timespec& time1_ts, const timespec& time2_ts)
+{
+	return (time1_ts.tv_sec < time2_ts.tv_sec || (time1_ts.tv_sec == time2_ts.tv_sec && time1_ts.tv_nsec < time2_ts.tv_nsec));
+}
+
+bool operator> (const timespec& time1_ts, const timespec& time2_ts)
+{
+	return (time1_ts.tv_sec > time2_ts.tv_sec || (time1_ts.tv_sec == time2_ts.tv_sec && time1_ts.tv_nsec > time2_ts.tv_nsec));
 }
